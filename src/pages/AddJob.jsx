c489cf4d83d090e3066acb6a4ce2ed7d2a45eee9
Malah-Ay <zafervrcic@gmail.com>
2024-09-22 13:46:07 +0300

    feature

--- src/pages/AddJob.jsx
+++ src/pages/AddJob.jsx
@@ -1,35 +1,58 @@
-import React from 'react'
+import React from "react";
+import { statusOpt, typeOpt } from "../constants";
 
 const AddJob = () => {
+  const handleSubmit= (e)=>{
+    e.preventDefault();
+
+    //to react inputs
+    const formData= new FormData(e.target);
+    const newJob=Object.fromEntries(formData.entries());
+    console.log(newJob)
+  }
   return (
-    <div className='add-page'>
-      <section className='add-sec'>
+    <div className="add-page">
+      <section className="add-sec">
         <h2>Yeni iş Ekle</h2>
-        <form action="">
+        <form onSubmit={handleSubmit}>
           <div>
             <label> Pozisyon</label>
-            <input name='position' type="text" required />
+            <input name="position" type="text" required />
           </div>
           <div>
             <label> Şirket</label>
-            <input name='company' type="text" required />
+            <input name="company" type="text" required />
           </div>
           <div>
             <label> Lokasyon</label>
-            <input name='location' type="text" required />
+            <input name="location" type="text" required />
           </div>
           <div>
             <label> Durum</label>
-            <select name="status" id=""></select>
+            <select name="status">
+              <option value="" hidden>Seçiniz</option>
+              {statusOpt.map((i) => (
+                <option>{i} </option>
+              ))}
+            </select>
           </div>
           <div>
             <label> Tür</label>
-            <select name="type" id=""></select>
+            <select name="type">
+              <option value="" hidden>Seçiniz</option>
+              {typeOpt.map((i) => (
+                <option>{i} </option>
+              ))}
+            </select>
+          </div>
+
+          <div className="button">
+            Add Job<span className="button-border"></span>
           </div>
         </form>
       </section>
     </div>
-  )
-}
+  );
+};
 
-export default AddJob
+export default AddJob;
